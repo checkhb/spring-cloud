22ccacde5ecc5182678be348048c5b6cb19ae313
hubin <503693631@qq.com>
2019-08-07 14:57:59 +0800

    add provider

---
+++ springcloud/eureka-provider1/src/main/java/com/hb/provider1/EurekaProvider1Application.java
@@ -0,0 +1,15 @@
+package com.hb.provider1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaProvider1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProvider1Application.class, args);
+    }
+
+}
---
+++ springcloud/eureka-provider2/src/main/java/com/hb/provider2/EurekaProvider2Application.java
@@ -0,0 +1,15 @@
+package com.hb.provider2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaProvider2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProvider2Application.class, args);
+    }
+
+}
